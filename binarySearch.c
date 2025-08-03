447278a0c88bdb507fcb795597c91eaafa22cfc4
Dipta-b <diptabanik0@gmail.com>
2025-08-03 15:37:22 +0600

    perfectly-done

--- binarySearch.c
+++ binarySearch.c
@@ -1,41 +1,59 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-int i,arr[100],x,n,start = 0,flags=0;
-printf("Enter the size of array :");
-scanf("%d",&n);
-int last=n-1;
-printf("Taking array elements :");
-for(i=0;i<n;i++){
-    scanf("%d",&arr[i]);
-}
-printf("Array elements are : ");
-for(i=0;i<n;i++){
-    printf("%d ",arr[i]);
-}
+int main()
+{
+    int i, x, n, arr[100], start = 0;
 
-    printf("\nItem You Want To Search? ");
-    scanf("%d", &x);
+    printf("Enter size of array: ");
+    scanf("%d", &n);
 
-    while(start <= last){
-        int middle = (start + last) /2;
-        if(arr[middle] == x){
-             printf("Item Found At Position : %d", middle + 1);
-             flags = 1;
-            return 0;
-        }
-        else if (arr[middle] < x){
-            start = middle +1;
-            flags = 1;
-        }
-        else {
-            last = middle - 1;
-            flags=1;
+    printf("Enter array elements: ");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for (i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
         }
     }
-if(flags == 0){
-    printf("The item not found");
-}
 
-return 0;
+    printf("Sorted array elements are: ");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    printf("\nEnter element to search: ");
+    scanf("%d", &x);
+
+    int last = n - 1;
+    while (start <= last)
+    {
+        int middle = (start + last) / 2;
+        if (arr[middle] == x)
+        {
+            printf("Item found at position %d\n", middle + 1);
+            return 0;
+        }
+        else if (arr[middle] < x)
+        {
+            start = middle + 1;
+        }
+        else
+        {
+            last = middle - 1;
+        }
+    }
+
+    printf("Item not found\n");
+    return 0;
 }
