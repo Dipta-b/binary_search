6274fa9e960373306d700f308759c5fdf56063f8
Dipta-b <diptabanik0@gmail.com>
2025-08-03 14:55:12 +0600

    updated-again

--- binarySearch.c
+++ binarySearch.c
@@ -1,37 +1,41 @@
-// Dipta Banik CSE2302029067
-// Binary Search
-#include <stdio.h>
-int main()
-{
+#include<stdio.h>
 
-    int arr[10] = {2, 5, 10, 35, 44, 49, 53, 57, 86, 96}, i, x, size = 10, start = 0, last = size - 1;
-    printf("Array Elements Are :");
-
-    for (i = 0; i < size; i++)
-    {
-        printf("%d ", arr[i]);
-    }
+int main(){
+int i,arr[100],x,n,start = 0,flags=0;
+printf("Enter the size of array :");
+scanf("%d",&n);
+int last=n-1;
+printf("Taking array elements :");
+for(i=0;i<n;i++){
+    scanf("%d",&arr[i]);
+}
+printf("Array elements are : ");
+for(i=0;i<n;i++){
+    printf("%d ",arr[i]);
+}
 
     printf("\nItem You Want To Search? ");
     scanf("%d", &x);
 
-    while (start <= last)
-    {
-        int middle = (start + last) / 2;
-        if (arr[middle] == x)
-        {
-            printf("Item Found At Position : %d", middle + 1);
+    while(start <= last){
+        int middle = (start + last) /2;
+        if(arr[middle] == x){
+             printf("Item Found At Position : %d", middle + 1);
+             flags = 1;
             return 0;
         }
-        else if (arr[middle] < x)
-        {
-            start = middle + 1;
+        else if (arr[middle] < x){
+            start = middle +1;
+            flags = 1;
         }
-        else
-        {
+        else {
             last = middle - 1;
+            flags=1;
         }
     }
-    printf("Item is not in the array");
-    return 0;
+if(flags == 0){
+    printf("The item not found");
+}
+
+return 0;
 }
